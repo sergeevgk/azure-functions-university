90f9c696c1e883c90f89c41a27fc24b97f19c1e2
sergeevgk <gosha199904@gmail.com>
2024-02-04 17:27:24 +0300

    Implement function with Route parameter for greeting.

---
+++ src/AzureFunctionsUniversity/HelloWorldHttpTriggerWithRoute.cs
@@ -0,0 +1,52 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Web;
+
+namespace AzureFunctionsUniversity
+{
+	public class HelloWorldHttpTriggerWithRoute
+	{
+		private readonly ILogger _logger;
+
+		public HelloWorldHttpTriggerWithRoute(ILoggerFactory loggerFactory)
+		{
+			_logger = loggerFactory.CreateLogger<HelloWorldHttpTriggerWithRoute>();
+		}
+
+		[Function(nameof(HelloWorldHttpTriggerWithRoute))]
+		public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, nameof(HttpMethod.Get), nameof(HttpMethod.Post), Route = "HelloWorldHttpTriggerWithRoute/{greeting:alpha?}")] 
+			HttpRequestData request,
+			string greeting)
+		{
+			_logger.LogInformation("HTTP trigger function processed a request.");
+
+			string name;
+			if (request.Method.Equals(nameof(HttpMethod.Get), StringComparison.OrdinalIgnoreCase))
+			{
+				var queryStringCollection = HttpUtility.ParseQueryString(request.Url.Query);
+				name = queryStringCollection["name"];
+			}
+			else
+			{
+				name = await request.ReadAsStringAsync();
+			}
+
+			var response = request.CreateResponse(HttpStatusCode.OK);
+
+			if (string.IsNullOrEmpty(name))
+			{
+				response.StatusCode = HttpStatusCode.BadRequest;
+				await response.WriteStringAsync("Please provide a value for the name query string parameter.");
+			}
+			else
+			{
+				response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+				await response.WriteStringAsync($"{greeting ?? "Hello"}, {name}!");
+			}
+
+			return response;
+		}
+	}
+}
