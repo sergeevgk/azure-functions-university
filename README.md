26d01263fd68a684c0b0436a7de0f65f3f6139d2
sergeevgk <gosha199904@gmail.com>
2024-02-11 00:44:40 +0300

    Implement 'dynamic binding' of path to the blob via sdk Blob client which is the only option for isolated worker. Update Readme with notes.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ It saves a lot of time because Microsoft.Azure.Functions.Worker.Extensions is th
 	HttpTrigger does not allow to use Custom type with **\[HttpTrigger(...)\]** attribute.
 It requires to pass an HttpRequest or HttpRequestData parameter with this **[HttpTrigger]** attribute and only then custom parameter with **[FromBody]** attribute can be passed. I didn't figure out why is that so, but in [Azure Functions University guide for .Net Core 3.1](https://github.com/marcduiker/azure-functions-university/blob/main/lessons/dotnetcore31/blob/README.md) there was a different flow **for in-process function**. Maybe that's what makes difference, but I didn't find anything in [Microsoft documentation for HttpTrigger](https://learn.microsoft.com/en-gb/azure/azure-functions/functions-bindings-http-webhook-trigger) or [Isolated Worker model](https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=windows) about it.
 
-2. Using dynamic Blob output bindings section:
-	This no longer works **for isolated worker model** as IBinder and Binder are members of Microsoft.Azure.WebJobs.
+2. Using dynamic Blob output bindings and CloudBlobContainer:
+	This no longer works **for isolated worker model** as IBinder and Binder, as well as CloudBlobContainer are members of Microsoft.Azure.WebJobs.
 	I did not find a way to implement this part of lesson, so it can be skipped if one works with latest version of .NET, I guess.
+	It is recommended to register and use appropriate Sdk Clients (e.g. BlobServiceClient) explicitly instead.
---
+++ src/AzureFrunctionsUniversity_BlobBinding/GetPlayerWithStringInputDynamic.cs
@@ -0,0 +1,56 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Logging;
+using System.Net.Mime;
+using System.Text;
+
+namespace AzureFrunctionsUniversity_BlobBinding
+{
+	public class GetPlayerWithStringInputDynamic
+	{
+		private readonly ILogger<GetPlayerWithStringInputDynamic> _logger;
+		private readonly BlobContainerClient _playerContainerClient;
+
+		public GetPlayerWithStringInputDynamic(ILogger<GetPlayerWithStringInputDynamic> logger, IAzureClientFactory<BlobServiceClient> blobClientFactory)
+		{
+			_logger = logger;
+			_playerContainerClient = blobClientFactory.CreateClient(StorePlayerWithContainerBlobOutput.PlayerStorageClientName).GetBlobContainerClient("players");
+			_playerContainerClient.CreateIfNotExists();
+		}
+
+		[Function(nameof(GetPlayerWithStringInputDynamic))]
+		public async Task<HttpResponseData> Run(
+			[HttpTrigger(AuthorizationLevel.Function,nameof(HttpMethods.Get), Route = null)] HttpRequestData request)
+		{
+			_logger.LogInformation("C# HTTP trigger function processed a request.");
+
+			HttpResponseData result;
+			string id = request.Query["id"];
+			if (string.IsNullOrEmpty(id))
+			{
+				var errorMessage = "No player data in request.";
+				result = request.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+				await result.WriteStringAsync(errorMessage);
+			}
+			else
+			{
+				var blobName = $"in/player-{id}.json";
+				var existingBlobClient = _playerContainerClient.GetBlobClient(blobName);
+				using var contentStream = await existingBlobClient.OpenReadAsync();
+				using var reader = new StreamReader(contentStream, encoding: Encoding.UTF8);
+				var content = await reader.ReadToEndAsync();
+				result = request.CreateResponse(System.Net.HttpStatusCode.OK);
+				// Instead of setting the header for content type explicitly, one can use WriteAsJsonAsync for JSON content.
+				// However, with existing example WriteAsJsonAsync adds backslashes before double quotes like \", which ruins the JSON.
+				// For the cases when raw JSON content is being read with StreamReader, it's worth adding a header but still writing content as string.
+				result.Headers.Add("Content-Type", MediaTypeNames.Application.Json);
+				await result.WriteStringAsync(content);
+			}
+
+			return result;
+		}
+	}
+}
