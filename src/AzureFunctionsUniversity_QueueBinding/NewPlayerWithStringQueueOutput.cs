63c86cf59dc9fab80ace403dbe4ae7dc00e36afb
sergeevgk <gosha199904@gmail.com>
2024-02-14 00:21:11 +0300

    1. Add QueueBinding project. 2. Introduce an HttpTrigger function with QueueOutput which sends a message with JSON body based on an entity provided in the body of POST request.

---
+++ src/AzureFunctionsUniversity_QueueBinding/NewPlayerWithStringQueueOutput.cs
@@ -0,0 +1,27 @@
+using AzureFrunctionsUniversity_QueueBinding.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctionsUniversity_QueueBinding
+{
+	public class NewPlayerWithStringQueueOutput
+	{
+		private readonly ILogger<NewPlayerWithStringQueueOutput> _logger;
+
+		public NewPlayerWithStringQueueOutput(ILogger<NewPlayerWithStringQueueOutput> logger)
+		{
+			_logger = logger;
+		}
+
+		[Function(nameof(NewPlayerWithStringQueueOutput))]
+		[QueueOutput(QueueConfig.NEW_PLAYER_ITEMS)]
+		public async Task<string> Run([HttpTrigger(AuthorizationLevel.Function, nameof(HttpMethod.Post))] HttpRequestData request, [FromBody] Player player)
+		{
+			_logger.LogInformation("C# HTTP trigger function processed a request.");
+			var response = JsonConvert.SerializeObject(player);
+			return response;
+		}
+	}
+}
---
+++ src/AzureFunctionsUniversity_QueueBinding/QueueConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace AzureFunctionsUniversity_QueueBinding
+{
+	public static class QueueConfig
+	{
+		public const string NEW_PLAYER_ITEMS = "newplayer-items";
+	}
+}
