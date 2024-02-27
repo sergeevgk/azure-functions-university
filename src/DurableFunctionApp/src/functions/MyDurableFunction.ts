346e762b85dad08b8cc9bbb7b8f2c6f5652e5fc5
sergeevgk <gosha199904@gmail.com>
2024-02-28 00:56:57 +0300

    The TS Durable Function project doesn't work for now. I tried to add durable functions project using TypeScript and VisualStudioCode but I failed to run it. The error "must use import to load es module" can probably be related to the Node version on my computer which I can't update right now. However, there is so many discussions on this issue on StackOverflow, that I do not want to continue with it.

---
+++ src/DurableFunctionApp/src/functions/MyDurableFunction.ts
@@ -0,0 +1,36 @@
+import { app, HttpHandler, HttpRequest, HttpResponse, InvocationContext } from '@azure/functions';
+import * as df from 'durable-functions';
+import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
+
+const activityName = 'MyDurableFunction';
+
+const MyDurableFunctionOrchestrator: OrchestrationHandler = function* (context: OrchestrationContext) {
+    const outputs = [];
+    outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
+    outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
+    outputs.push(yield context.df.callActivity(activityName, 'Cairo'));
+
+    return outputs;
+};
+df.app.orchestration('MyDurableFunctionOrchestrator', MyDurableFunctionOrchestrator);
+
+const MyDurableFunction: ActivityHandler = (input: string): string => {
+    return `Hello, ${input}`;
+};
+df.app.activity(activityName, { handler: MyDurableFunction });
+
+const MyDurableFunctionHttpStart: HttpHandler = async (request: HttpRequest, context: InvocationContext): Promise<HttpResponse> => {
+    const client = df.getClient(context);
+    const body: unknown = await request.text();
+    const instanceId: string = await client.startNew(request.params.orchestratorName, { input: body });
+
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+    return client.createCheckStatusResponse(request, instanceId);
+};
+
+app.http('MyDurableFunctionHttpStart', {
+    route: 'orchestrators/{orchestratorName}',
+    extraInputs: [df.input.durableClient()],
+    handler: MyDurableFunctionHttpStart,
+});
