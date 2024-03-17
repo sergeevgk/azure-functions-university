c5af4ed66dcf7cc740bb3894c1ba14ce8d1845d4
sergeevgk <gosha199904@gmail.com>
2024-03-18 02:45:16 +0300

    Implement Azure Maps search using Azure.Maps preview SDK. Register a client as a singleton service according to MS recommendations about SDK clients lifetime management (it's better to use factory, but for learning project singleton is enough). https://devblogs.microsoft.com/azure-sdk/lifetime-management-and-thread-safety-guarantees-of-azure-sdk-net-clients/ NewtonsoftJson and GeoJson do not work with Azure.Core.GeoJson.GeoPosition so I passed plain double values serialized to string as an interim result.

--- src/AzureFunctionsUniversity_DurableFunctions/AzureMapSearchInput.cs
+++ src/AzureFunctionsUniversity_DurableFunctions/AzureMapSearchInput.cs
@@ -4,8 +4,17 @@ namespace AzureFunctionsUniversity_DurableFunctions;
 
 public class AzureMapSearchInput
 {
+	/// <summary>
+	/// URL Encoded search address query.
+	/// Example: 1600%20Amphitheatre%20Parkway%2C%20Mountain%20View%2C%20CA
+	/// </summary>
 	[JsonPropertyName("searchAddress")]
 	public string SearchAddress { get; set; }
-	[JsonPropertyName("nusinessEntityType")]
+
+	/// <summary>
+	/// URL Encoded entity name.
+	/// Example:juice%20bar
+	/// </summary>
+	[JsonPropertyName("businessEntityType")]
 	public string BusinessEntityType { get; set; }
 }
--- src/AzureFunctionsUniversity_DurableFunctions/AzureMapsSearch.cs
+++ src/AzureFunctionsUniversity_DurableFunctions/AzureMapsSearch.cs
@@ -1,26 +1,36 @@
+using Azure.Core.GeoJson;
+using Azure.Maps.Search;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.DurableTask;
 using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System.Web;
 
 namespace AzureFunctionsUniversity_DurableFunctions
 {
-	public static class AzureMapsSearch
+	public class AzureMapsSearch
 	{
 		const int FIRST_RETRY_INTERVAL_MS = 1000;
 		const int MAX_RETRY_INTERVAL_MS = 1000;
 		const int RETRY_TIMEOUT_MS = 7000;
 		const int MAX_NUMBER_OF_ATTEMPTS = 3;
+		private readonly MapsSearchClient _client;
+
+		public AzureMapsSearch(MapsSearchClient client)
+		{
+			_client = client ?? throw new ArgumentNullException(nameof(client));
+		}
 
 		[Function(nameof(AzureMapsSearch))]
-		public static async Task<List<string>> RunOrchestrator(
+		public async Task<List<string>> RunOrchestrator(
 			[OrchestrationTrigger] TaskOrchestrationContext context)
 		{
 			ILogger logger = context.CreateReplaySafeLogger(nameof(AzureMapsSearch));
 			logger.LogInformation("Saying hello.");
-			
+
 			var retryOptions = TaskOptions.FromRetryPolicy(new RetryPolicy(
 				MAX_NUMBER_OF_ATTEMPTS,
 				TimeSpan.FromMilliseconds(FIRST_RETRY_INTERVAL_MS),
@@ -28,19 +38,19 @@ namespace AzureFunctionsUniversity_DurableFunctions
 				TimeSpan.FromMilliseconds(MAX_RETRY_INTERVAL_MS),
 				TimeSpan.FromMilliseconds(RETRY_TIMEOUT_MS)));
 
-
 			var input = context.GetInput<AzureMapSearchInput>();
 			var outputs = new List<string>();
 			try
 			{
 				// find location by address
 				var location = await context.CallActivityAsync<string>(nameof(FindLocationByAddressAsync), input.SearchAddress, retryOptions);
-
+				var positionValues = location.Trim('[', ']').Split(',').Select(double.Parse).ToArray();
 				// perform local search 
 				var localSearchInput = new LocalSearchInput
 				{
 					BusinessEntityName = input.BusinessEntityType,
-					Location = "123"
+					Longitude = positionValues[0],
+					Latitude = positionValues[1]
 				};
 				var localSearchResult = await context.CallActivityAsync<IEnumerable<string>>(nameof(LocalSearchByEntityAsync), localSearchInput, retryOptions);
 				outputs = localSearchResult.ToList();
@@ -55,38 +65,48 @@ namespace AzureFunctionsUniversity_DurableFunctions
 		}
 
 		[Function(nameof(FindLocationByAddressAsync))]
-		public static async Task<string> FindLocationByAddressAsync([ActivityTrigger] string address, FunctionContext executionContext)
+		public async Task<string> FindLocationByAddressAsync([ActivityTrigger] string address, FunctionContext executionContext)
 		{
 			ILogger logger = executionContext.GetLogger(nameof(FindLocationByAddressAsync));
 			logger.LogInformation($"Searching for {address}");
+			var urlEncodedAddress = HttpUtility.UrlEncode(address);
+			var searchResponse = await _client.SearchAddressAsync(urlEncodedAddress);
+			
+			var resultPosition =  searchResponse.Value.Results.FirstOrDefault()?.Position ?? default;
+			var result = resultPosition.ToString();
 
-			return "";
+			return result;
 		}
 
 		public record LocalSearchInput
 		{
-            public string BusinessEntityName { get; set; }
-            public string Location { get; set; }
-        }
+			public string BusinessEntityName { get; set; }
+			public double Longitude { get; set; }
+			public double Latitude { get; set; }
+		}
 
 		[Function(nameof(LocalSearchByEntityAsync))]
-		public static async Task<IEnumerable<string>> LocalSearchByEntityAsync([ActivityTrigger] LocalSearchInput input, FunctionContext executionContext)
+		public async Task<IEnumerable<string>> LocalSearchByEntityAsync([ActivityTrigger] LocalSearchInput input, FunctionContext executionContext)
 		{
 			ILogger logger = executionContext.GetLogger(nameof(LocalSearchByEntityAsync));
-			logger.LogInformation($"Searching for {input.BusinessEntityName} in local area {input.Location}");
+			logger.LogInformation($"Searching for {input.BusinessEntityName} in local area [{input.Longitude:G5},{input.Latitude:G5}]");
 
-			var result = new List<string>()
+			var searchOptions = new SearchPointOfInterestOptions
 			{
-				"kaffeterija",
-				"baristocratia",
-				"coffeefactory"
+				Coordinates = new GeoPosition(input.Longitude, input.Latitude),
+				RadiusInMeters = 1000,
+				Top = 5
 			};
+
+			var urlEncodedBusinessEntityName = HttpUtility.UrlEncode(input.BusinessEntityName);
+			var localSearchResult = await _client.SearchPointOfInterestAsync(urlEncodedBusinessEntityName, options: searchOptions); 
+			var result = localSearchResult.Value.Results.Select(lsr => $"{lsr.PointOfInterest.Name}, {lsr.Address.Country}, {lsr.Address.FreeformAddress}").ToList();
 
 			return result;
 		}
 
 		[Function($"{nameof(AzureMapsSearch)}_HttpStart")]
-		public static async Task<HttpResponseData> HttpStart(
+		public async Task<HttpResponseData> HttpStart(
 			[HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData request,
 			[DurableClient] DurableTaskClient client,
 			FunctionContext executionContext)
